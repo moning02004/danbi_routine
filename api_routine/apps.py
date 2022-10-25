c8cc9aefd984f50bb8ff11e446614d7f5565c983
jeonghoonyu <moning02004@naver.com>
2022-10-25 22:34:59 +0900

    Revert and Add routine app

--- api_routine/apps.py
+++ api_routine/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AppRoutineConfig(AppConfig):
+class ApiRoutineConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_routine'
+    name = 'api_routine'
