1cf889819d6e527b09b5803d37b3ac9dd9d89ab0
jeonghoonyu <moning02004@naver.com>
2022-10-30 16:49:43 +0900

    테스트를 위해 JWT access 토큰 유효기간 변경

--- danbi_project/settings.py
+++ danbi_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +26,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-# Application definition
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
 APPEND_SLASH = False
 AUTH_USER_MODEL = "api_user.Account"
 REST_FRAMEWORK = {
